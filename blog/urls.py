6a818a4d7ea14e3b7de71f9d27c9a4f04c054317
akhila <akhilalkashyap98@gmail.com>
2019-09-19 10:29:29 +0530

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.main,name='main'),
     path('',views.home,name='home'),
+    path('',views.upload,name='upload'),
+    path('',views.feedback,name='feedback'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def main(request):
-    return render(request,'blog/main.html',{})
+    posts = Post.objects.filter(published_date_lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/main.html',{'posts': posts})
 
 
 def home(request):
-    return render(request,'blog/home.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/home.html',{'posts': posts})
 
 def upload(request):
-    return render(request, 'blog/upload.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/upload.html',{'posts': form})
 
 def feedback(request):
-    return render(request, 'blog/feedback.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/feedback.html',{'posts': form})
 
     
